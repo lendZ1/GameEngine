e57255d59b351cbd80bde42ec1fe1baf33af68fd
lendZ1 <krognes.erlend@gmail.com>
2025-07-15 13:14:07 +0200

    1

---
+++ BevegendePanel.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class BevegendePanel extends JPanel {
+    private int xpos;
+    private int ypos;
+    public BevegendePanel(int xpos, int ypos) {
+        this.xpos = xpos;
+        this.ypos = ypos;
+    } 
+
+}
