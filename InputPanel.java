3edfa6337198ba648001a047f584d147733da2a2
lendZ1 <krognes.erlend@gmail.com>
2025-07-20 13:05:48 +0200

    lagde InputPanel.java

---
+++ InputPanel.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.concurrent.BrokenBarrierException;
+
+public class InputPanel extends BevegendePanel {
+    public InputPanel(int xpos, int ypos) {
+        super(xpos, ypos);
+        setBackground(Color.LIGHT_GRAY);
+        setBounds(xpos, ypos, 200, 100); // Set size and position
+    }
+}
+
+    
