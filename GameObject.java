363a5f3840840f0a8ec17e68521cedbbbf68496d
lendZ1 <krognes.erlend@gmail.com>
2025-07-24 15:54:06 +0200

    gameobject->firkant

--- GameObject.java
+++ GameObject.java
@@ -12,20 +12,28 @@ public class GameObject implements Runnable {
     private int yfart=0;
     private Color farge;
 
+    private int høyde, bredde;
 
-    public GameObject(int xpos, int ypos) {
+
+    public GameObject(int xpos, int ypos, int høyde, int bredde, Color farge) {
         this.xpos = xpos;
         this.ypos = ypos;
-    } 
-
-    public void settFarge(Color farge) {
+        this.høyde = høyde;
+        this.bredde = bredde;
         this.farge = farge;
-    }
+    } 
 
     public void run(){  //må legge til en barrier latch her
         while (true){
             xpos += xfart;
             ypos += yfart;
+            //tegn(Game.vindu.getGraphics()); // Tegner GameObject på vinduet
         } 
+    }
+
+
+    private void tegn(Graphics g) { // Metode for å tegne GameObject på vinduet
+        g.setColor(farge);
+        g.fillRect(xpos, ypos, bredde, høyde);
     }
 }
