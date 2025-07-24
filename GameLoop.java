c677d6f01d3c073528f974c33dfa6ba81c90b089
lendZ1 <krognes.erlend@gmail.com>
2025-07-24 20:43:40 +0200

    Update GameLoop.java

--- GameLoop.java
+++ GameLoop.java
@@ -11,9 +11,26 @@ public class GameLoop implements Runnable {
 
     @Override
     public void run() {
+        long lastTime = System.nanoTime();
+        long now;
+        double delta = 0;
 
+        while (true) {
+            now = System.nanoTime();
+            delta += (now - lastTime) / (double) TIME_PER_TICK;
+            lastTime = now;
+
+            while (delta >= 1) {
+                panel.update();     // update game logic
+                panel.repaint();    // request a redraw
+                delta--;
+            }
+
+            try {
+                Thread.sleep(2); // optional: prevents CPU overload
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-
-    
 }
