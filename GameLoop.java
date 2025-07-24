91cca0e86744d01000ab0e088aa1521c665c5ae6
lendZ1 <krognes.erlend@gmail.com>
2025-07-24 20:35:18 +0200

    Update GameLoop.java

--- GameLoop.java
+++ GameLoop.java
@@ -1,6 +1,8 @@
 
 
 public class GameLoop implements Runnable {
+    private static final int TICK_RATE = 60; // Ticks per second
+    private static final int TICK_INTERVAL = 
     private Game game;
 
     public GameLoop(Game game) {
@@ -9,7 +11,7 @@ public class GameLoop implements Runnable {
 
     @Override
     public void run() {
-        
+
     }
 
 
