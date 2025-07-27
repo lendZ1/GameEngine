1d404cba3bb7ccb203c66ea3db798c774a0e8bc9
lendZ1 <krognes.erlend@gmail.com>
2025-07-27 12:49:02 +0200

    strarter gameloop tråd

--- Main.java
+++ Main.java
@@ -5,5 +5,8 @@ class Main{
     public static void main(String args[]){
         Game game = new Game();
         GameLoop gameLoop = new GameLoop(game);
+
+        Thread gameThread = new Thread(gameLoop);   //starter game loop i en egen tråd
+        gameThread.start();
     }
 }
