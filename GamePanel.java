10f854e3f7ff93c942e8c0f5575ed25355ae7a92
lendZ1 <krognes.erlend@gmail.com>
2025-07-27 15:34:58 +0200

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -1,26 +1,41 @@
 import java.awt.*;
 import javax.swing.*;
-import java.util.TreeMap;
+import java.util.ArrayList;
 
 
 public class GamePanel extends JPanel {
-    public TreeMap<Integer, GameObject> gameObjects;    //Holder GameObjects og hvilket lagsnivå de er på
+    private ArrayList<GameObject> GO1;   //holder det nederste laget med GameObjects "bakgrunnen"
+    private ArrayList<GameObject> GO2;  //holder det midterste laget med GameObjects
+    private ArrayList<GameObject> GO3;  //holder det øverste laget med GameObjects
+
     public GamePanel() {
         super();
         setPreferredSize(new Dimension(800, 600));
-        gameObjects = new TreeMap<>();
+        gameObjects = new ArrayList<>();
     }
     public void update() {
-        for (GameObject obj : gameObjects.values()) {
-            obj.oppdaterPosisjon(); // Update position of each GameObject
+        for (GameObject obj : GO1) {   //Oppdaterer posisjonen til alle GameObjects
+            obj.oppdaterPosisjon(); 
+        }
+        for (GameObject obj : GO2) {
+            obj.oppdaterPosisjon();
+        }
+        for (GameObject obj : GO3) {
+            obj.oppdaterPosisjon();
         }
     }
 
     @Override
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {    // Tegner alle GameObjects på skjermen
         super.paintComponent(g);
-        for (GameObject obj : gameObjects.values()) {
-            obj.tegn(g); // Draw each GameObject
+        for (GameObject obj : GO1) {
+            obj.tegn(g);
+        }
+        for (GameObject obj : GO2) {
+            obj.tegn(g);
+        }
+        for (GameObject obj : GO3) {
+            obj.tegn(g);
         }
     }
 }
