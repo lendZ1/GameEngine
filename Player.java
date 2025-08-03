dbf1a4afd74692e6610f9ee8cad4739cdb5c4d7e
lendZ1 <krognes.erlend@gmail.com>
2025-08-03 18:44:25 +0200

    player

--- Player.java
+++ Player.java
@@ -1,13 +1,12 @@
-
+import java.awt.*;
+import javax.swing.*;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.ArrayList;
 
 public class Player extends GameObject {
     public Player(GameMap gamemap, int xpos, int ypos) {
         super(gamemap, xpos, ypos, 50, 50, Color.BLUE, 1);
-        bounce = false; // Player does not bounce off walls
+        setBounce(false); // Player does not bounce off walls
     }
 
-    @Override
-    public void oppdaterPosisjon() {
-        super.oppdaterPosisjon();
-    }
 }
