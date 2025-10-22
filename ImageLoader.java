723fcffc421d11a6e5450e08a30b0dd193e9534a
lendZ1 <krognes.erlend@gmail.com>
2025-10-22 22:41:18 +0200

    bufferedImage scaling
    method in IMaageLoader to automaticly for scaling the image

--- ImageLoader.java
+++ ImageLoader.java
@@ -2,6 +2,9 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
 
 public class ImageLoader{
     public static BufferedImage load (String filePath){
@@ -12,4 +15,19 @@ public class ImageLoader{
             return null;
         }
     }
+
+    public static BufferedImage scale(BufferedImage original, int width, int height) {
+        // Create a new BufferedImage with the desired size
+        BufferedImage scaled = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = scaled.createGraphics();
+
+        // Choose the interpolation method (for pixel art, use NEAREST_NEIGHBOR)
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+
+        // Draw the scaled image
+        g2d.drawImage(original, 0, 0, width, height, null);
+        g2d.dispose();
+
+        return scaled;
+        }
 }
