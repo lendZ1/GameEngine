231ddb5f3dd82bde2076735e53bced24d7f814dd
lendZ1 <krognes.erlend@gmail.com>
2025-11-19 21:14:38 +0100

    Revert "Created logic and gameobject package"
    This reverts commit 67f723a19af7eb160142c25b573a6fb8d5a6e6a1.

--- GameMap.java
+++ GameMap.java
@@ -1,5 +1,3 @@
-package Logic; 
-
 import java.util.ArrayList;
 import java.util.TreeMap;
 import java.awt.Graphics;
--- GamePanel.java
+++ GamePanel.java
@@ -1,5 +1,3 @@
-package Logic; 
-
 import java.awt.*;
 import javax.swing.*;
 import java.util.ArrayList;
