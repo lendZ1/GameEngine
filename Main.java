bb554c3594b109836dc67dc4878d0aa4c8445dc0
Erlend Krognes <krognes.erlend@gmail.com>
2026-03-09 21:37:55 +0100

    fullscreen

--- Main.java
+++ Main.java
@@ -5,9 +5,19 @@ import Engine.Tools.*;
 import Engine.UI.*;
 
 class Main{
-    static int width = 800;
-    static int height = 600;
     public static void main(String args[]){
+        //logic to open the window as fullscreen
+        boolean fullscreen = false;
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width, height;
+        if (fullscreen){
+            width = (int) screenSize.getWidth();
+            height = (int) screenSize.getHeight();
+        } else {
+            width = 800;
+            height = 600;
+        }
+
         GWindow window = new GWindow();
         StartMenu startMenu = new StartMenu(window, width, height);
         window.add(startMenu);
